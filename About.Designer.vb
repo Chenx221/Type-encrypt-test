6c690d91d2344c86d609334771578f707e256752
Chenx221 <jiaxinghuoyan@163.com>
2018-09-22 21:08:57 +0800

    Add files via upload

---
+++ About.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class About
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(97, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(186, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Back"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(147, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(179, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "By Chenx221" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Blog:https://chenx221.ml/blog"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(147, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(167, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Ver:0.01" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Build Time: 2018/9/22 18:04"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(43, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(78, 82)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'About
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(395, 126)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "About"
+        Me.Text = "关于"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+    Dim minwen As String
+    Dim anhao As String
+    Dim miwen As String
+
+    Dim anhao_long As Integer
+    Dim mw_long As Integer
+    Dim miwen_long As Integer
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        About.Show()
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        miwen = TextBox3.Text
+        anhao = TextBox2.Text
+        Dim ah_asc__3 As Long
+        Dim pwd1 As String
+        Dim wait_to_uncode As String
+        Dim wait_uncode As String
+        Dim uncode As String
+        uncode = ""
+        wait_uncode = ""
+        pwd1 = ""
+        For u = 1 To Len(anhao)
+            ah_asc__3 = Asc(Mid(anhao, u, 1)) + 3
+            pwd1 = pwd1 + Chr(ah_asc__3)
+        Next
+
+        wait_to_uncode = Replace(miwen, pwd1, "")
+
+        For ip = 1 To Len(wait_to_uncode) Step 4
+            wait_uncode = Mid(wait_to_uncode, ip, 4)
+            uncode = uncode + Chr(Asc(Replace(wait_uncode, "@", "", 2, -1)) - 2)
+        Next
+        TextBox1.Text = uncode
+
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        minwen = TextBox1.Text
+        anhao = TextBox2.Text
+        miwen = TextBox3.Text
+
+        mw_long = Len(minwen)
+        anhao_long = Len(anhao)
+        miwen_long = Len(miwen)
+
+        Dim j As Integer
+        Dim pwd As String
+        Dim ah_asc_3 As Long
+        pwd = ""
+        For j = 1 To anhao_long
+            ah_asc_3 = Asc(Mid(anhao, j, 1)) + 3
+            pwd = pwd + Chr(ah_asc_3)
+        Next
+
+        '此处的pwd用来填充在原密文的首部
+
+
+        Dim i As Integer
+        Dim miwen_1time As String
+        Dim mw(0 To i + 5) As String
+        Dim b As String
+        Dim c As Long
+        Dim d As Long
+        Dim g As String
+        Dim f As String
+        Dim encrypt_word As String
+        miwen_1time = ""
+        For i = 1 To mw_long
+            b = Mid(minwen, i, 1)
+            C = Asc(b)
+            d = c + 2
+            g = Chr(d)
+            f = g
+
+            If Len(f) = 1 Then
+                f = "@@@" + f
+            ElseIf Len(f) = 2 Then
+                f = "@@" + f
+            ElseIf Len(f) = 3 Then
+                f = "@" + f
+            Else
+            End If
+
+            miwen_1time = miwen_1time + f
+        Next
+
+        encrypt_word = pwd + miwen_1time
+        TextBox3.Text = ""
+        TextBox3.Text = encrypt_word
+
+    End Sub
+End Class
